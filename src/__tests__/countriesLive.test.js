20d9b14f75014b5a127244eb3a3e52390f578b7c
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-03-15 12:13:55 +0200

    Add test for getAll function in countriesLive.test.js

--- src/__tests__/countriesLive.test.js
+++ src/__tests__/countriesLive.test.js
@@ -1,16 +1,19 @@
 import countries from "../services/countries"
 
 test("getALL returns all countries from the API", async () => {
+    // call the getAll function from the countries service
     const data = await countries.getAll()
+
+    // check that the data is an array
     expect(Array.isArray(data)).toBe(true)
 
+    // check that the data is not empty
     data.forEach(country => {
         expect(country).toHaveProperty("name")
         expect(country).toHaveProperty("population")
         expect(country).toHaveProperty("flags")
         expect(country).toHaveProperty("area")
     })
-
 })
 
 
---
+++ src/__tests__/countriesSlice.test.js
@@ -0,0 +1,41 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countriesService from "../services/countries";
+import countriesSlice, { initializeCountries } from "../store/countriesSlice";
+
+jest.mock("../services/countries");
+
+describe("countriesSlice tests", () => {
+    let store;
+    beforeEach(() => {
+        store = configureStore({
+            reducer: {
+                countries: countriesSlice
+            }
+        })
+
+    });
+
+    it("should initialize countries in the store", () => {
+        const { countries, isLoading } = store.getState().countries;
+        expect(countries).toHaveLength(0);
+        expect(isLoading).toBe(true);
+    });
+    it("should handle countries", () => {
+        store.dispatch({
+            type: "countries",
+            payload: ["country 1", "country 2"]
+        });
+        expect(store.getState().countries.countries).toEqual(["country 1", "country 2"]);
+        expect(store.getState().countries.isLoading).toEqual(false);
+    });
+    it("should handle initialCountries", async () => {
+        const mockCountries = ["country 1", "country 2"];
+
+        countriesService.getAll.mockResolvedValue(mockCountries);
+
+        await store.dispatch(initializeCountries());
+        expect(store.getState().countries.countries).toEqual(mockCountries);
+        expect(store.getState().countries.isLoading).toEqual(false);
+
+    })
+})
